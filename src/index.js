12eec89caf89133811bcd22e1fd8e66005fd98b3
Martin Najjar <martin_najjar@yahoo.com>
2021-01-15 22:04:35 +0300

    Setting up the right URL

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 require('./css/styles.css');
 
 let myCity = 'baghdad';
-let myUrl = `api.weatherapi.com/v1/current.json?key=bd962442b08549bca00173111211501&q=London`;
 
 async function fetchApi(cityName) {
   try {
     const result = await fetch(
-      'api.weatherapi.com/v1/current.json?key=bd962442b08549bca00173111211501&q=London'
+      `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.API_KEY}`
     );
-    console.log(result);
+    const data = await result.json();
+    console.log(data);
   } catch (err) {
     console.log(err);
   }
 }
-// anotherFunc(myCity);
+
 fetchApi(myCity);
-// console.log(fetchApi());
-// console.log(fetchApi());
