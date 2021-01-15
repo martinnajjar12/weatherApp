c211c342db212596811b8abe55db338f2642db8e
Martin Najjar <martin_najjar@yahoo.com>
2021-01-15 23:13:49 +0300

    Grab the data from json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 require('./css/styles.css');
 
+const description = document.querySelector('.main');
+const iconTag = document.querySelector('#iconWeather');
+const mainTemp = document.querySelector('.mainTemp');
+const maxTemp = document.querySelector('.maxTemp');
+const minTemp = document.querySelector('.minTemp');
+const windSpeed = document.querySelector('.windSpeed');
+const cityElement = document.querySelector('.cityName');
 let myCity = 'baghdad';
+
+function fillElements({ name, weather, main, wind }) {
+  cityElement.textContent = name;
+  description.textContent = weather[0].description;
+  icon = weather[0].icon;
+  iconTag.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  mainTemp.textContent = main.temp;
+  maxTemp.textContent = main.temp_max;
+  minTemp.textContent = main.temp_min;
+  windSpeed.textContent = wind.speed;
+}
 
 async function fetchApi(cityName) {
   try {
@@ -8,7 +26,7 @@ async function fetchApi(cityName) {
       `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.API_KEY}`
     );
     const data = await result.json();
-    console.log(data);
+    fillElements(data);
   } catch (err) {
     console.log(err);
   }
